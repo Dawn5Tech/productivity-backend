fa0e116e87f2e4a470f4c8daad46e407ec4d2e29
Dawn5Tech <dawnmbakara@gmail.com>
2026-01-05 20:10:49 +0100

    Fix Render proxy and host settings

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, NoteViewSet
+from .views import CategoryViewSet, NoteViewSet,obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
@@ -8,4 +8,5 @@ router.register(r'notes', NoteViewSet, basename='note')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), #added using chatgpt
 ]
