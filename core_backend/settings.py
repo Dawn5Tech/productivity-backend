5c79b4c1c21ff0ce932a8dd0689c8ba027d98686
Dawn5Tech <dawnmbakara@gmail.com>
2026-01-05 17:11:12 +0100

    Fix Render proxy and host settings

--- core_backend/settings.py
+++ core_backend/settings.py
@@ -88,13 +88,30 @@ WSGI_APPLICATION = 'core_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
+#DATABASES = {
+    #'default':{
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        default=os.getenv('DATABASE_URL')
-    )
-}
+    #}
+#}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # Password validation
