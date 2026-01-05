a7652d0e5f1b6dce2fc70c7ea6339fa3704830c8
Dawn5Tech <dawnmbakara@gmail.com>
2026-01-05 19:29:22 +0100

    Fix Render proxy and host settings

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
@@ -9,4 +10,5 @@ router.register(r'', TaskViewSet, basename='task')
 # 2. The API URLs are now determined automatically by the router
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
