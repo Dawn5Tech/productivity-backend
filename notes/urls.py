67cd89bda25959cac2b38dc33572edae3d2d9163
Dawn5Tech <dawnmbakara@gmail.com>
2026-01-05 20:22:46 +0100

    Fix Render proxy and host settings

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, NoteViewSet,obtain_auth_token
+from .views import CategoryViewSet, NoteViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
