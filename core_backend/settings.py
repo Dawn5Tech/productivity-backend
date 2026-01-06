d9e7654e822b41b8079da707d3cd97adaa19df9c
Dawn5Tech <dawnmbakara@gmail.com>
2026-01-06 08:57:54 +0100

    Fix Render proxy and host settings

--- core_backend/settings.py
+++ core_backend/settings.py
@@ -99,7 +99,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.config(
-            default=DATABASE_URL,
+            #default=DATABASE_URL,
             conn_max_age=600,
             ssl_require=True,
         )
